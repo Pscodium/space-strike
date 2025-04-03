5f0bb90a03a85b0dfcec92d1f95de3959672a5b0
Pscodium <peter-larson@hotmail.com>
2025-04-03 14:30:05 -0300

    fix: environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
-})
+    server: {
+        port: Number(process.env.PORT) || 2304,
+        host: process.env.HOST || 'localhost',
+        fs: {
+            allow: ['.'],
+        },
+    },
+    assetsInclude: ['**/*.md'],
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
+});
